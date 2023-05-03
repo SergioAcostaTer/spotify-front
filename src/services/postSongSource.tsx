3e9c4ae4ac1a73f22de972370c3024d7c7604454
Sergio Acosta <sergioacostaquintana@gmail.com>
2023-05-04 00:05:00 +0100

    ready build shissssssss pls

--- src/services/postSongSource.tsx
+++ src/services/postSongSource.tsx
@@ -1,17 +1,22 @@
-
 export default async function postSongSource(song: any) {
   try {
     console.log(song);
-    const response = await fetch(`https://spsotify-back-ok.onrender.com/sourceMusic`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(song),
-    });
+    const response = await fetch(
+      `https://spsotify-back-ok.onrender.com/sourceMusic`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(song),
+      }
+    );
     if (response.ok) {
       const data = await response.json();
       console.log(data);
+      data?.audio?.allData?.map((song: any) => {
+        console.log(song);
+      });
       return data;
     } else {
       throw new Error(`Request failed with status ${response.status}`);
