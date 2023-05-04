eb5a709c2f9a9fcd12c10df44b0e35ed6d518418
Sergio Acosta <sergioacostaquintana@gmail.com>
2023-05-04 16:39:44 +0100

    kinda cool

--- src/services/postSongSource.tsx
+++ src/services/postSongSource.tsx
@@ -1,34 +1,24 @@
-async function blobToBase64(blob: Blob) {
-  return new Promise((resolve, _) => {
-    const reader = new FileReader();
-    reader.onloadend = () => resolve(reader.result);
-    reader.readAsDataURL(blob);
-  });
-}
-
 export default async function postSongSource(song: any) {
   const t0 = performance.now();
 
   try {
     console.log(song);
-    const response = await fetch(`https://spsotify-back-ok.onrender.com/sourceMusic`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(song),
-    });
-
-    //time to get response
+    const response = await fetch(
+      `https://spsotify-back-ok.onrender.com/sourceMusic`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(song),
+      }
+    );
 
     if (response.ok) {
       const data = await response.json();
 
       const t1 = performance.now();
       console.log(`Call to postSongSource took ${t1 - t0} milliseconds.`);
-
-      console.log(data);
-
       return data;
     } else {
       throw new Error(`Request failed with status ${response.status}`);
